9c0d212e1bf411a81734a20a1e207d87c2b16e22
gongbi00 <82161453+gongbi00@users.noreply.github.com>
2021-12-14 23:08:04 +0900

    Add files via upload

---
+++ calculate_calories.py
@@ -0,0 +1,11 @@
+def calculate(weight, tall, age, sex):
+    a = 10 * weight
+    b = 6.25*tall
+    c = 5*age
+    if sex = 1: #남자
+        return a + b - c  + 5
+    elif sex = 2:
+        return a + b - c -151
+    else:
+        return 0
+
